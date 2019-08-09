a6053ab9250f0a192c876bcb130312a28a1c453e
yaoyu bao <yaobao5354@gmail.com>
2019-08-09 19:07:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" ", " ",]
+board = [" ", " ", " ", " ",]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}\n
          #{board[3]} | #{board[4]} | #{board[5]}\n
