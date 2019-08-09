ae2179a620494b688365ac8dc8424f81392f6c52
yaoyu bao <yaobao5354@gmail.com>
2019-08-09 19:05:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,8 @@
 # out the current state.
 
 def display_board(board)
-  puts " #{board[0]} | #{board[0]} | #{board[0]}\n
-         #{board[0]} |
+  puts " #{board[0]} | #{board[1]} | #{board[2]}\n
+         #{board[3]} | #{board[4]} | #{board[5]}\n
+         #{board[6]} | #{board[7]} | #{board[8]}"
 end
+
